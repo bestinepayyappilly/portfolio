9f0821781ac947871a00047481bf0f4d2527ab5b
bestinepayyappilly <bestine1234@gmail.com>
2026-02-04 14:10:37 +0530

    fix: adjust layout dimensions and improve grid properties in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,19 +67,20 @@ export default function RootLayout({
         className={cn(
           "min-h-screen bg-background font-sans antialiased relative",
           geist.variable,
-          geistMono.variable
+          geistMono.variable,
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="light">
           <TooltipProvider delayDuration={0}>
-            <div className="absolute inset-0 top-0 left-0 right-0 h-[100px] overflow-hidden z-0">
+            <div className="absolute inset-0 top-0 left-0 right-0 h-[400px] overflow-hidden z-0">
               <FlickeringGrid
                 className="h-full w-full"
-                squareSize={2}
-                gridGap={2}
+                squareSize={5}
+                gridGap={5}
                 style={{
                   maskImage: "linear-gradient(to bottom, black, transparent)",
-                  WebkitMaskImage: "linear-gradient(to bottom, black, transparent)",
+                  WebkitMaskImage:
+                    "linear-gradient(to bottom, black, transparent)",
                 }}
               />
             </div>
